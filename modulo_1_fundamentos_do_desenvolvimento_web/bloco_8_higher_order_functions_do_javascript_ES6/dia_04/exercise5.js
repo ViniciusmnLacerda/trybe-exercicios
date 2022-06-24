1fdb71bc3ec3882249c92891bbc9a91244be0fa2
Vinícius Lacerda <viniciuslmn@gmail.com>
2022-06-24 16:52:10 -0300

    exercicios 8.4

---
+++ modulo_1_fundamentos_do_desenvolvimento_web/bloco_8_higher_order_functions_do_javascript_ES6/dia_04/exercise5.js
@@ -0,0 +1,20 @@
+const names = [
+  'Aanemarie', 'Adervandes', 'Akifusa',
+  'Abegildo', 'Adicellia', 'Aladonata',
+  'Abeladerco', 'Adieidy', 'Alarucha',
+];
+
+
+function containsA() {
+  return names.reduce((acc, curr) => {
+    return acc + curr.split('').reduce((acumulator, current) => {
+      if (current === 'a' || current === 'A') {
+        return acumulator + 1;
+      }
+      return acumulator;
+    },0);
+  },0);
+}
+
+console.log(containsA());
+
