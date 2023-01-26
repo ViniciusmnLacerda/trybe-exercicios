93c925b57ea090512ab798a488c84b2543eea164
Vinícius Lacerda <viniciuslmn@gmail.com>
2023-01-26 14:04:32 -0300

    30.1

--- modulo_3_desenvolvimento_back_end/bloco_30_APIs_OO_e_NoSQL/dia_02/poo-trix/src/Interfaces/IPayment.ts
+++
@@ -1,9 +0,0 @@
-interface IPayment {
-  id?: string
-  payByPerson: string;
-  payToPerson: string;
-  amount: number;
-  key: string;
-}
-
-export default IPayment;
--- modulo_3_desenvolvimento_back_end/bloco_30_APIs_OO_e_NoSQL/dia_02/poo-trix/src/utils/PaymentStatus.ts
+++
@@ -1,7 +0,0 @@
-enum PaymentStatus {
-  pending = 0,
-  concluded = 1,
-  reversed = 2,
-}
-
-export default PaymentStatus;
