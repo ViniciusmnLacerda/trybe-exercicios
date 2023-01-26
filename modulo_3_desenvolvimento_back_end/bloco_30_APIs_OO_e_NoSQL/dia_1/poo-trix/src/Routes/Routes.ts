64a200501855eef63d9ed7e6f7bb7deab9496fc9
Vinícius Lacerda <viniciuslmn@gmail.com>
2023-01-26 14:05:09 -0300

    30.1

---
+++ modulo_3_desenvolvimento_back_end/bloco_30_APIs_OO_e_NoSQL/dia_1/poo-trix/src/Routes/Routes.ts
@@ -0,0 +1,20 @@
+import { Router } from 'express';
+import TransferController from '../Controllers/TransferController';
+
+const routes = Router();
+
+routes.post(
+  '/transfer',
+  (req, res, next) => new TransferController(req, res, next).create(),
+);
+
+routes.get(
+  '/transfer',
+  (req, res, next) => new TransferController(req, res, next).getAll(),
+);
+
+routes.get(
+  '/transfer/:key',
+  (req, res, next) => new TransferController(req, res, next).getByKey(),
+);
+export default routes;
